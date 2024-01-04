d250aa5cd15e480a194f3dd3a4969d243831ea5a
KhoyumMasalik <abdulkuyummasalik@smkwikrama.sch.id>
2024-01-04 11:37:00 +0700

    4 Januari 2024

--- script.js
+++ script.js
@@ -119,3 +119,61 @@
 // }
 
 // console.log("Penjumlahan dari 1 + 2 + 3 + 4 + " + angka + " adalah: " + faktorial);
+
+// //4-1-2024
+// //faktorial for
+// let angka = 5;
+// let faktorial = 1;
+
+// if (angka < 0) {
+//   console.log("Input Harus Non-Negatif");
+// } else {
+//   for (let i = 2; i <= angka; i++) {
+//     faktorial *= i;
+//   }
+//   console.log(`Faktorial dari ${angka} adalah : ${faktorial}`);
+// }
+
+// //faktorial while
+// let angka = 5;
+// let faktorial = 1;
+
+// if (angka < 0) {
+//   console.log("Input Harus Non-Negatif");
+// } else {
+//   let i = 1;
+//   while (i <= angka) {
+//     faktorial *= i;
+//     i++;
+//   }
+//   console.log(`Faktorial dari ${angka} adalah : ${faktorial}`);
+// }
+
+//faktorial do-while
+let angka = 5;
+let faktorial = 1;
+
+if (angka < 0) {
+  console.log("Input Harus Non-Negatif");
+} else {
+  let i = 1;
+  do {
+    faktorial *= i;
+    i++;
+  } while (i <= angka);
+  console.log(`Faktorial dari ${angka} adalah : ${faktorial}`);
+}
+
+// //Mencari tahun kabiset
+// var tahun = "";
+// for (let tahun = 1600; tahun <= 2024; tahun++) {
+//   if (tahun % 4 === 0 && tahun !== 1800 && tahun != 1700 && tahun != 1900) {
+//     console.log(tahun + " adalah tahun kabisat.");
+//   } else if (tahun % 100 !== 0) {
+//     console.log(tahun + " bukan tahun kabisat.");
+//   } else if (tahun % 400 === 0) {
+//     console.log(tahun + " adalah tahun kabisat.");
+//   } else {
+//     console.log(tahun + " bukan tahun kabisat.");
+//   }
+// }
